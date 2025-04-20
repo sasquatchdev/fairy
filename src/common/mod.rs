c7511410ae54730579c4d1567d1707c6a31f5da7
sasquatchdev <about@sasquatchdev.me>
2025-04-21 00:47:13 +0200

    feat: project structure

---
+++ src/common/mod.rs
@@ -0,0 +1,8 @@
+/// module that contains code which
+/// deals with error handling.
+pub mod errs;
+
+/// module that contains logging 
+/// (initialization) code as well
+/// as some error and warning messages.
+pub mod logs;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+/// module that contains common code,
+/// which is shared accross the entire
+/// project. 
+/// this includes things like the error
+/// handling system, logging, configuration,
+/// etc.
+pub mod common;
+
+/// module that contains all built-in
+/// components.
+/// these include very basic components like
+/// transform, camera, etc. but also more
+/// advanced components regarding physics,
+/// rendering, audio and more.
+pub mod components;
+
+/// module that contains code regarding
+/// the entity component system (ecs).
+/// this includes entities, components and
+/// systems and therefore resembles the
+/// highest level of abstraction.
+pub mod system;
+
+/// module that contains code which abstracts
+/// away the raw opengl and glfw calls (which are
+/// inherently unsafe) and provides a higher level,
+/// safe abstraction.
+pub mod display;
+
 fn main() {
     println!("Hello, world!");
 }
